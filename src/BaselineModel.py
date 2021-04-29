d8613692e7d988218a1fc9f2576910bbabb2478b
Marc Merih <55166987+marcmerih@users.noreply.github.com>
2021-04-29 14:33:36 -0400

    updated readability

--- src/BaselineModel.py
+++ src/BaselineModel.py
@@ -16,38 +16,57 @@ import numpy as np
 class BaselineModel(BaseEstimator):
     def __init__(self, l2_beta=0.001, kernel='linear', gamma=0.1, loss_name='hinge', lambda_max=1, max_iter=3000, solver='SCS', verbose=False,reason_points=0.5):
 
-        self.l2_beta = l2_beta # Regularization parameter beta for the l2 regularization
-        self.kernel = kernel # The SVM kernel to be used.. Options:['linear','rbf','poly']
-        self.gamma = gamma # If kernel='rbf', gamma to be kernel width, If kernel='poly', gamma to be degree.
-        self.loss_name = loss_name # Loss function to be used. Options:['hinge','logistic','squared','exponential']
-        self.lambda_max = lambda_max # The max lambda value for the start of the binary search.
-        self.max_iter = max_iter # The number of iterations.
-        self.solver = solver # The solver to be used by cvxpy. Options:['SCS','ECOS'].
-        self.verbose = verbose # If true, Overrides the default of hiding solver output.
-        self.reason_points = reason_points # The ratio of points used as reasonable points for the similarity-based approach of SearchFair.
+        #   L2 Regularization parameter beta
+        self.l2_beta = l2_beta
+        
+        #   The SVM kernel can be 'linear', 'rbf', or 'poly'. 
+        self.kernel = kernel
+        
+        #   if kernel='rbf', then Gamma = kernel_width. Else if kernel='poly', then Gamma = degree
+        self.gamma = gamma
+        
+        #   Loss function can be 'hinge', 'logistic', 'squared', or 'exponential'.
+        self.loss_name = loss_name
+        
+        #   Initial lambda_max value for beginning of binary search.
+        self.lambda_max = lambda_max
+        
+        #   Maximum number of iterations to be completed.
+        self.max_iter = max_iter
+        
+        #   CVXPY's solver to be used, which can either be 'SCS', or 'ECOS'.
+        self.solver = solver
+        
+        #   If verobe is set to true, default of hiding the solver output will be overriden.
+        self.verbose = verbose
+        
+        #   In the SearchFair's similarity based approach: the percentage of points to be used as 'reasonable points'
+        self.reason_points = reason_points
 
     def fit(self, x_train, y_train,s_train):
 
-        # Set class variables
+        #   Setting of the class variables
         self.x_train = x_train
         self.y_train = y_train
         self.s_train = s_train
         
-        # Must call preprocess() after the datapoint size is given
+        #   preprocess() upon receiving data_point size
         self._preprocess()
-        # Construct the CVXPY problem
+        
+        #   Construction of the CVXPY problem.
         self._construct_problem()
-        # Optimize with given settings 
+        
+        #   Carry out optimization.
         self._optimize()
         
         return self
 
     def _preprocess(self):
        
-        # Initialize the coef_ array as a class variable using cvxpy's built-in loss functions
+        #   Using CVXPY's pre-built loss functions, initialize the coef_ array as a class variable.
         self.coef_ = None
 
-        # Generate loss function based on the given loss_name
+        #   Using the loss_name provided, generate the respective loss function
         if self.loss_name == 'logistic':
             self.loss_func = lambda z: cp.logistic(-z)
         elif self.loss_name == 'hinge':
@@ -57,7 +76,8 @@ class BaselineModel(BaseEstimator):
         elif self.loss_name == 'exponential':
             self.loss_func = lambda z: cp.exp(-z)
         else:
-            self.loss_func = lambda z: cp.pos(1.0 - z) # Use Hinge-Loss unless specified otherwise
+            #   If no loss name provided OR different loss name than those defined, then use Hinge-Loss.
+            self.loss_func = lambda z: cp.pos(1.0 - z)
 
         if self.kernel == 'rbf':
             self.kernel_function = lambda X, Y: kernels.rbf_kernel(X, Y, self.gamma)
@@ -69,7 +89,7 @@ class BaselineModel(BaseEstimator):
             self.kernel_function = kernel
 
 
-        # Choose random reasonable points
+        #   Random reasonable points chosen:
         self.nmb_pts = len(self.s_train)
         if self.reason_points <= 1:
             self.reason_pts_index = list(range(int(self.nmb_pts * self.reason_points)))
@@ -79,17 +99,18 @@ class BaselineModel(BaseEstimator):
 
     def _construct_problem(self):
 
-        # Variable to optimize
+        #   Set the params variable to be optimized
         self.params = cp.Variable((len(self.reason_pts_index), 1))
-        # Parameter for Kernel Matrix
+        
+        #   The Kernel Matrix to be set 
         self.kernel_matrix = cp.Parameter(shape=(self.x_train.shape[0], len(self.reason_pts_index)))
         
         self.bias = cp.Variable()
 
-        # Loss Function to Minimize (with Regularization)
+        #   The regularized loss function that we will minimize
         self.loss = (1 / self.nmb_pts) * cp.sum(self.loss_func(cp.multiply(self.y_train.reshape(-1, 1), self.kernel_matrix @ self.params))) + self.l2_beta * cp.square(cp.norm(self.params, 2))
         
-        # Final Problem Formulization
+        #   The final formulation of the problem.
         self.prob = cp.Problem(cp.Minimize(self.loss))
 
     def _optimize(self):
@@ -109,7 +130,8 @@ class BaselineModel(BaseEstimator):
     
     def predict(self, x_test):
         kernel_matrix = self.kernel_function(x_test, self.x_train[self.reason_pts_index])
-        # Calculate Estimates
+        
+        # Calculation of the estimates
         y_hat = np.dot(self.coef_, np.transpose(kernel_matrix))
         
         return np.sign(y_hat)
